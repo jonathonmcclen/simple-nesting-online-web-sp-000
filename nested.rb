c2c476aec740386fa96b4dc80595476a49f7346a
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-12 20:21:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -117,7 +117,6 @@ def changing_alan
 end
 
 def adding_to_dennis
-  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -133,6 +132,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[]
      
 
 end
